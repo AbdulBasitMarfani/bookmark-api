ec6ffb38c3d259ae77398ecfd87ee0863c5b7b41
Abdul Basit Marfani <basitmarfani@outlook.com>
2025-04-26 20:32:51 +0500

    Initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: 'User signed up successfully!' };
+  }
+  login() {
+    return { msg: 'User logged in successfully!' };
+  }
+}
